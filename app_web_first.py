460b1200030ced0eecb21781d95736f6a1972fd8
rajeevmouli <34650756+rajeevmouli@users.noreply.github.com>
2026-04-04 08:55:17 -0400

    Update app_web_first.py

--- app_web_first.py
+++ app_web_first.py
@@ -1,11 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods["GET","POST"])
 def home():
-    return render_template("index.html")
+    name = None
+    if request.method = "POST":
+       name = request.form.get("username") 
+     
+    return render_template("index.html",name=name)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
