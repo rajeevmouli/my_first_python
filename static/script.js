5506207856d768430b2d869bb62c58acd20ef091
rajeevmouli <34650756+rajeevmouli@users.noreply.github.com>
2026-04-05 10:40:19 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,15 @@
-function sayHello() {
-    document.getElementById("message").innerText =
-        "✅ JavaScript is working!";
+function sendName() {
+    const name = document.getElementById("nameInput").value;
+
+    fetch("/hello", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name: name })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("message").innerText = data.message;
+    });
 }
