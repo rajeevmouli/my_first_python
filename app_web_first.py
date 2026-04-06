8de6c83e4821119fc28525600109fd32a66c1d78
rajeevmouli <34650756+rajeevmouli@users.noreply.github.com>
2026-04-05 20:39:30 -0400

    Update app_web_first.py

--- app_web_first.py
+++ app_web_first.py
@@ -17,11 +17,18 @@ def hello():
     name = data.get("name", "Guest")
     return jsonify({"message": f"Hello {name}! 🚀"})
 
+@app.route("/")
+def home():
+    return render_template("index.html", title="Home")
 
-    
 @app.route("/about")
-def about(): 
-    return render_template("about.html")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html", title="Skills")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
